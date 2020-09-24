925822717dc2b423aa7bc8a3cddf0cbc6c296687
ambercs18 <53806443+ambercs18@users.noreply.github.com>
2020-09-24 15:34:33 +0530

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -3,10 +3,12 @@ read a
 echo "Enter 2nd number"
 read b
 sum=`echo $a + $b|bc`
+diff=`echo $a - $b|bc`
 pro=`echo $a \* $b|bc`
 q=`echo $a / $b|bc`
 mod=`echo $a % $b|bc`
 echo "Sum is "$sum
+echo "Difference is "$diff
 echo "Product is "$pro
 echo "Quotient is "$q
 echo "Remainder is "$mod
