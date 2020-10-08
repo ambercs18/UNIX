503ad8325c79bd925b76036a1af89822dc19eb10
ambercs18 <53806443+ambercs18@users.noreply.github.com>
2020-10-08 14:52:21 +0530

    Create checkForX.sh

---
+++ checkForX.sh
@@ -0,0 +1,13 @@
+echo "Enter a number"
+read x
+y=0
+
+if [ $x -eq $y ]
+then
+echo "Number is zero"
+elif [ $x -gt $y ]
+then
+echo "Number is greater than zero"
+else
+echo "Number is less than zero"
+fi
